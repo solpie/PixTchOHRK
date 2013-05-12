115114218cb61aa639971d4b593c1516e6877b71
solpie <solpie.net@gmail.com>
2013-05-12 22:44:52 +0800

    uppo

--- pixtch/uppo/views.py
+++ pixtch/uppo/views.py
@@ -1,1 +1,20 @@
 __author__ = 'SolPie'
+
+from flask import Blueprint, render_template
+from models import Uppo
+
+route_uppo = Blueprint('uppo', __name__, template_folder='../templates/pixtch/uppo')
+
+
+def get_uppo(pid):
+    return Uppo.query.filter(Uppo.id == pid).first()
+
+
+@route_uppo('/p/<int:pid>')
+def uppo_view(pid):
+    if pid:
+        uppo = get_uppo(pid)
+        return render_template('detail.html', uppo=uppo)
+    else:
+        render_template('list.html')
+
