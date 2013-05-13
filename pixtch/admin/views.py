5110663bedf2d39ca686ba1f42be9cfc11566ba2
solpie <solpie.net@gmail.com>
2013-05-13 23:40:28 +0800

    uppo table

--- pixtch/admin/views.py
+++ pixtch/admin/views.py
@@ -19,8 +19,9 @@ class BackendView(BaseView):
     @expose('/')
     def index(self):
         url = url_for('.test')
-        return self.render('index.html', url=url)
-        # return self.render('pixtch/admin/index.html')
+        print 'BackendView'
+        # return self.render('index.html', url=url)
+        return self.render('pixtch/admin/index.html')
 
     @expose('/test/')
     def test(self):
@@ -30,6 +31,9 @@ class BackendView(BaseView):
 from auth.models import User
 # Customized User model admin
 class UserAdmin(ModelView):
+    list_template = 'admin/user/list.html'
+    create_template = 'admin/user/create.html'
+    edit_template = 'admin/user/edit.html'
     # Show only name and email columns in list view
     column_list = ('name', 'email', 'register_date', 'last_login_date')
 
@@ -52,9 +56,8 @@ def init_admin(app):
     from database import db_session
     from kn.models import KnPost, KnCategory, Tag
 
-    admin = Admin(name='Pixtch Backend')
-    # admin = Admin(app, name='Pixtch Backend')
-    admin.add_view(BackendView(name='Pixtch Backend'))
+    admin = Admin(name='Pixtch', base_template='admin/user/layout.html')
+    admin.add_view(BackendView(name='Pixtch Backend', endpoint='testadmin'))
     admin.add_view(UserAdmin(User, db_session, category='User'))
     admin.add_view(KnPostAdmin(KnPost, db_session, category='Kn'))
     admin.add_view(ModelView(Tag, db_session, category='Kn'))
--- pixtch/uppo/models.py
+++ pixtch/uppo/models.py
@@ -8,10 +8,19 @@ from auth.models import User
 class Uppo(Base):
     __tablename__ = 'uppo'
     id = Column(Integer, primary_key=True)
-    skill = Column(String)
-    name_p = Column(String)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relation(User, backref=backref('uppo', order_by=id))
+    name_p = Column(String)
+    sex = Column(Integer)
+    birthday = Column(DateTime)
+    cellphone = Column(Integer)
+    name_real = Column(String)
+    qq = Column(Integer)
+    skill = Column(String)
+    avatar = Column(String)
+    weibo_id = Column(Integer)
+    brief = Column(String)
+
 
     def __repr__(self):
         return '<Uppo %r>' % self.name_p
