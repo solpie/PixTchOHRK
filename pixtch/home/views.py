bd502a8c7fd8242661e794f40628ec9f8d611ffb
solpie <solpie.net@gmail.com>
2013-05-14 23:10:45 +0800

    piapro look

--- pixtch/home/views.py
+++ pixtch/home/views.py
@@ -26,6 +26,14 @@ def test_view():
     return 'test'
 
 
+@route_home.route('/images/<regex("[a-zA-Z0-9]+.jpg"):img>')
+@route_home.route('/images/<regex(".+.jpg"):img>')
+@route_home.route('/images/<regex(".+.png"):img>')
+def image(img):
+    imgs = str(img)
+    return redirect(url_for('static', filename='images/' + imgs))
+
+
 @route_home.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
