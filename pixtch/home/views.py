57c0fb418610cea190049a5225623b39247d9ef0
solpie <solpie.net@gmail.com>
2013-05-14 23:32:23 +0800

    piapro look

--- pixtch/home/views.py
+++ pixtch/home/views.py
@@ -26,9 +26,10 @@ def test_view():
     return 'test'
 
 
-@route_home.route('/images/<regex("[a-zA-Z0-9]+.jpg"):img>')
+# @route_home.route('/images/<regex("[a-zA-Z0-9]+.jpg"):img>')
 @route_home.route('/images/<regex(".+.jpg"):img>')
 @route_home.route('/images/<regex(".+.png"):img>')
+@route_home.route('/images/<regex(".+.gif"):img>')
 def image(img):
     imgs = str(img)
     return redirect(url_for('static', filename='images/' + imgs))
