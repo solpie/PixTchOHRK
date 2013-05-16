a7559f84010d6b3ed616a015fa1da5c18fa5442e
solpie <solpie.net@gmail.com>
2013-05-16 09:37:07 +0800

    run on sae

--- deploy/bae/pixtch/index.py
+++ deploy/bae/pixtch/index.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'SolPie'
-from bae.core.wsgi import WSGIApplication
-
 import sys
 import os
-
 
 from bae.core.const import *
 
@@ -20,6 +17,9 @@ print "This is BAE environ"
 
 sys.path.insert(0, os.path.join('.', 'site-packages'))
 from runserver import app
+
 app.setup()
-# app.init_db(MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_PORT, 'MVwslEoiYLtUAerKTSuE')
+
+from bae.core.wsgi import WSGIApplication
+
 application = WSGIApplication(app)
